6c903bdac31cfbee6dd7e34463a6846af3ca5580
Rafael Dalzotto <rdalzotto@itba.edu.ar>
2022-09-03 18:46:37 -0300

    Delete test/build directory

--- test/build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/rafdal/Documentos/PlatformIO/Projects/lib-development/lib/lib-data-table/test
-# Build directory: /home/rafdal/Documentos/PlatformIO/Projects/lib-development/lib/lib-data-table/test/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
